05dc9a2d029602dc184ee1d0cedcaecc9eed61e6
James Bond <james.bond312@hotmail.com>
2019-03-22 20:42:59 +0000

    Added in a CacheService.

---
+++ src/PlexRequests.Helpers/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PlexRequests.Helpers
+{
+    public interface ICacheService
+    {
+        Task<T> GetOrCreate<T>(string cacheKey, Func<Task<T>> createFunc);
+        void Remove(string cacheKey);
+    }
+}
--- src/PlexRequests.Settings/SettingsService.cs
+++ src/PlexRequests.Settings/SettingsService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using PlexRequests.Helpers;
 using PlexRequests.Store;
 
 namespace PlexRequests.Settings
@@ -6,25 +7,38 @@ namespace PlexRequests.Settings
     public class SettingsService : ISettingsService
     {
         private readonly ISettingsRepository _settingsRepository;
+        private readonly ICacheService _cacheService;
+        private const string CacheKey = "Settings";
 
-        public SettingsService(ISettingsRepository settingsRepository)
+        public SettingsService(ISettingsRepository settingsRepository, ICacheService cacheService)
         {
             _settingsRepository = settingsRepository;
+            _cacheService = cacheService;
         }
 
         public async Task<Store.Models.Settings> Get()
         {
-            return await _settingsRepository.GetSettings();
+            return await _cacheService.GetOrCreate(CacheKey,
+                createFunc: async () => await _settingsRepository.GetSettings());
+
         }
 
         public async Task Save(Store.Models.Settings settings)
         {
-            await _settingsRepository.UpdateSettings(settings);
+            await _cacheService.GetOrCreate(CacheKey, async () =>
+            {
+                await _settingsRepository.UpdateSettings(settings);
+                return await _settingsRepository.GetSettings();
+            });
         }
 
         public async Task PrimeSettings(Store.Models.Settings settings, bool overwrite)
         {
-            await _settingsRepository.PrimeSettings(settings, overwrite);
+            await _cacheService.GetOrCreate(CacheKey, async () =>
+            {
+                await _settingsRepository.PrimeSettings(settings, overwrite);
+                return await _settingsRepository.GetSettings();
+            });  
         }
     }
 }
