2a293a9a77494b0a3bafc40f63fce358dfe545fd
James Bond <James@Emilys-MacBook-Pro.local>
2019-03-23 21:50:36 +0000

    Updated property case names in PlexModels.

--- src/PlexRequests.Plex/Models/Directory.cs
+++ src/PlexRequests.Plex/Models/Directory.cs
@@ -9,5 +9,19 @@ namespace PlexRequests.Plex.Models
         public int ViewedLeafCount { get; set; }
         public string Key { get; set; }
         public string Title { get; set; }
+        public bool AllowSync { get; set; }
+        public string Art { get; set; }
+        public string Composite { get; set; }
+        public bool Filters { get; set; }
+        public bool Refreshing { get; set; }
+        public string Type { get; set; }
+        public string Agent { get; set; }
+        public string Scanner { get; set; }
+        public string Language { get; set; }
+        public string Uuid { get; set; }
+        public int UpdatedAt { get; set; }
+        public int CreatedAt { get; set; }
+        public int ScannedAt { get; set; }
+        public List<Location> Location { get; set; }
     }
 }
