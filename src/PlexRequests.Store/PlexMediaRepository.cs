2aa1ea0f8be5e92378cecfb7a899ec95284a81db
James Bond <james.bond312@hotmail.com>
2019-03-29 20:51:05 +0000

    Added PlexMediaRepository

---
+++ src/PlexRequests.Store/PlexMediaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PlexRequests.Store.Models;
+
+namespace PlexRequests.Store
+{
+    public class PlexMediaRepository: BaseRepository<PlexMediaItem>, IPlexMediaRepository
+    {
+        public PlexMediaRepository(string connectionString, string databaseName) : base(connectionString, databaseName, "PlexMediaItems")
+        {
+        }
+
+        public async Task CreateMany(IEnumerable<PlexMediaItem> mediaItems)
+        {
+            await Collection.InsertManyAsync(mediaItems);
+        }
+    }
+}
