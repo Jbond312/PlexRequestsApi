2907e06d539d40a9019916fee0c0fd37839c153d
James Bond <james.bond312@hotmail.com>
2019-03-29 21:04:34 +0000

    Move TheMovieDb options to it's own settings section

--- src/PlexRequests.TheMovieDb/TheMovieDbApi.cs
+++ src/PlexRequests.TheMovieDb/TheMovieDbApi.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using PlexRequests.Api;
 using PlexRequests.Settings;
 using PlexRequests.TheMovieDb.Models;
@@ -9,23 +10,26 @@ namespace PlexRequests.TheMovieDb
 {
     public class TheMovieDbApi : ITheMovieDbApi
     {
-        private const string BaseUri = "https://api.themoviedb.org/3/";
         private const string Region = "GB";
         private const string LanguageCode = "en-GB";
 
+        private readonly string _baseUri;
+        private readonly string _apiKey;
         private readonly IApiService _apiService;
-        private readonly ISettingsService _settingsService;
 
-        public TheMovieDbApi(IApiService apiService, ISettingsService settingsService)
+        public TheMovieDbApi(
+            IApiService apiService, 
+            IOptions<TheMovieDbSettings> settings)
         {
             _apiService = apiService;
-            _settingsService = settingsService;
+            _baseUri = settings.Value.BaseUri;
+            _apiKey = settings.Value.ApiKey;
         }
 
         public async Task<List<MovieSearch>> SearchMovies(string query, int? year, int? page)
         {
-            var requestBuilder = new ApiRequestBuilder(BaseUri, "search/movie", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var requestBuilder = new ApiRequestBuilder(_baseUri, "search/movie", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("query", query)
                 .AddQueryParam("page", GetPage(page));
 
@@ -43,8 +47,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<MovieSearch>> UpcomingMovies(int? page)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, "movie/upcoming", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, "movie/upcoming", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("region", Region)
                 .AddQueryParam("language", LanguageCode)
                 .AddQueryParam("page", GetPage(page))
@@ -57,8 +61,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<MovieSearch>> PopularMovies(int? page)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, "movie/popular", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, "movie/popular", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("region", Region)
                 .AddQueryParam("language", LanguageCode)
                 .AddQueryParam("page", GetPage(page))
@@ -71,8 +75,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<MovieSearch>> TopRatedMovies(int? page)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, "movie/top_rated", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, "movie/top_rated", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("region", Region)
                 .AddQueryParam("language", LanguageCode)
                 .AddQueryParam("page", GetPage(page))
@@ -85,8 +89,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<MovieDetails> GetMovieDetails(int movieId)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, $"movie/{movieId}", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, $"movie/{movieId}", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("language", LanguageCode)
                 .Build();
 
@@ -97,8 +101,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<TvSearch>> SearchTv(string query, int? page)
         {
-            var requestBuilder = new ApiRequestBuilder(BaseUri, "search/tv", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var requestBuilder = new ApiRequestBuilder(_baseUri, "search/tv", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("query", query)
                 .AddQueryParam("page", GetPage(page));
 
@@ -111,8 +115,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<TvSearch>> PopularTv(int? page)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, "tv/popular", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, "tv/popular", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("language", LanguageCode)
                 .AddQueryParam("page", GetPage(page))
                 .Build();
@@ -124,8 +128,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<List<TvSearch>> TopRatedTv(int? page)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, "tv/top_rated", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, "tv/top_rated", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("language", LanguageCode)
                 .AddQueryParam("page", GetPage(page))
                 .Build();
@@ -137,8 +141,8 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<TvDetails> GetTvDetails(int tvId)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, $"tv/{tvId}", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, $"tv/{tvId}", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("language", LanguageCode)
                 .Build();
 
@@ -149,21 +153,14 @@ namespace PlexRequests.TheMovieDb
 
         public async Task<TvSeasonDetails> GetTvSeasonDetails(int tvId, int seasonNumber)
         {
-            var apiRequest = new ApiRequestBuilder(BaseUri, $"tv/{tvId}/season/{seasonNumber}", HttpMethod.Get)
-                .AddQueryParam("api_key", await GetApiKey())
+            var apiRequest = new ApiRequestBuilder(_baseUri, $"tv/{tvId}/season/{seasonNumber}", HttpMethod.Get)
+                .AddQueryParam("api_key", _apiKey)
                 .AddQueryParam("language", LanguageCode)
                 .Build();
 
             var tvSeasonDetails = await _apiService.InvokeApiAsync<TvSeasonDetails>(apiRequest);
 
             return tvSeasonDetails;
-        }
-
-        private async Task<string> GetApiKey()
-        {
-            var settings = await _settingsService.Get();
-
-            return settings.TheMovieDbApiKey;
         }
 
         private static string GetPage(int? page)
