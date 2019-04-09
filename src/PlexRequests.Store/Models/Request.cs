3cb52cf3e273dd1a6c24df17037c5fcb1e104e29
James Bond <James@Emilys-MacBook-Pro.local>
2019-04-09 20:24:08 +0100

    Updated Dictionary to be represented correctly in MongoDb

--- src/PlexRequests.Store/Models/Request.cs
+++ src/PlexRequests.Store/Models/Request.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
 using PlexRequests.Store.Enums;
 
 namespace PlexRequests.Store.Models
@@ -15,6 +16,7 @@ namespace PlexRequests.Store.Models
         public int? PlexRatingKey { get; set; }
         public Guid RequestedByUserId { get; set; }
         public string RequestedByUserName { get; set; }
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
         public Dictionary<int, List<RequestEpisode>> SeasonEpisodes { get; set; }
         public bool IsApproved { get; set; }
     }
