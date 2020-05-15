e0605d9f9eef3fc8ab75e03c7329334ecccb69fa
James Bond <james@bonton.io>
2020-05-15 22:50:20 +0100

    Added 'StayWarm' timer trigger.

---
+++ src/PlexRequests.Functions/HttpTriggers/StayWarmTrigger.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace PlexRequests.Functions.HttpTriggers
+{
+    public class StayWarmTrigger
+    {
+        [FunctionName("StayWarm")]
+        // ReSharper disable once UnusedParameter.Global
+        public static void StayWarm([TimerTrigger("*/15 * * * * *")]TimerInfo timer)
+        {
+            //Runs every 15 mins
+        }
+    }
+}
